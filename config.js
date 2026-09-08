73339804053afaa9e3adeeb5fb465c128f814f46
andrewhong0629 <andrew980629hong@gmail.com>
2026-09-08 23:04:34 +0800

    feat: add Supabase frontend config

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase 公開前端設定；只放 publishable/anon key，絕對不要放 service_role key。
+window.SUPABASE_CONFIG = {
+  url: 'https://peeypxyncskipfafuihe.supabase.co',
+  anonKey: 'sb_publishable_gsWbmtX3p8eSXAmRlYlTDA_XovzQmr-'
+};
